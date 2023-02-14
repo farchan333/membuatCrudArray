b159380b42fdc652b9968650c61bd8daf0e38002
muhammad farchan thamrin <Mfarchan32@gmail.com>
2023-02-14 11:49:22 +0800

    menambahkan Entity

---
+++ Todolist.java
@@ -0,0 +1,18 @@
+public class Todolist {
+    private String nama;
+
+    public Todolist(String nama) {
+        this.nama = nama;
+    }
+
+    public Todolist() {
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
